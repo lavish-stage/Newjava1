e7e1a8fd0ae9c4c5ea5360c0e909c7c993d91200
lavish <shamim.zabit4@gmail.com>
2022-05-26 20:54:51 -0700

    Initial version

---
+++ src/Methods.java
@@ -0,0 +1,16 @@
+public class Methods {
+public static void main(String[] args) {
+
+    multiply(7,8);
+
+   /* when we say multiply we are just calling the multiply
+   method and then the comma ould mean multiply 5 and 10.
+    */
+
+}
+
+public static void multiply(int a, int b) {
+    System.out.println(a * b);
+
+}
+}
--- src/com/zabit/Main.java
+++ src/com/zabit/Main.java
@@ -1,7 +1,10 @@
 package com.zabit;
-
+/*
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Locale;
+
+//import static java.math.BigDecimal.multiply;
 
 public class Main {
 public static void main(String[] args){
@@ -30,7 +33,7 @@ public static void main(String[] args){
         and can only be used for whole numbers
          */
     /* example*/
-    int number = 5;
+    //int number = 5;
         /* the "number" is the varible and 5 is the value
         you're putting in.
          */
@@ -40,7 +43,7 @@ public static void main(String[] args){
        String   < is the data type for words
         */
     //example
-    String n = "zabit";
+   /* String n = "zabit";
     /* the n would be the varible
     and zabit would be the value
     for Strings you would have to put
@@ -59,7 +62,7 @@ public static void main(String[] args){
      should print out because i only printed the varible N.
      If I wanted to print out another varible then I would use
      the SOUT command again and call that varible*/
-    System.out.println(n);
+    /*System.out.println(n);
     /* Primitive data type is where you store
     single words and simple numbers
  ex: int-whole numbers, string-words, double-numbers +decimals, long.
@@ -67,7 +70,7 @@ public static void main(String[] args){
     to find out the date or time.
      */
     // Example for referance data type
-    Date date = new Date();
+  /*  Date date = new Date();
     /* anytime you see the word
     "new" its a Referance data type or its complex
     when you used this you might see an import statement at the very
@@ -76,7 +79,7 @@ public static void main(String[] args){
      again you can use the SOUT command and
      call the varible to see the date
      */
-    System.out.println(date);
+   /* System.out.println(date);
 
     //primitive data type
  /* 1. byte
@@ -84,34 +87,34 @@ example: you cannot give the
 varible the same word as "byte"
 byte stores whole numbers from -128-127
   */
-    byte thebyte = 126;
+    /*byte thebyte = 126;
 
 /* 2.short
  Short holds whole numbers from-32000-32000
   */
-    short theshort = 32000;
+    /* short theshort = 32000;
     /* 3. int
     holds whole numbers from -2mil-2mil
      */
-    int numbers = 2000000;
+   /* int numbers = 2000000;
     /* 4. long
 stores whole numbers from -9trillion-9trillion
      */
-    long bignumbers = 900000000;
+   /* long bignumbers = 900000000;
     /* 5. double
     stores fractional number as well as 15 decimal degits.
      */
-    double fraction = 3 / 8;
+   /* double fraction = 3 / 8;
     double decimal = 9.888888;
     /* 6. boolean
     store either true or false values
      */
-    boolean fa = false;
+   /* boolean fa = false;
     /* 7.char
     stores single characters/ letters.
      have to use single quotes for this
      */
-    char characters = 'F';
+   /* char characters = 'F';/*
 
 
     /* REFRENCE TYPE DATA
@@ -124,7 +127,7 @@ stores whole numbers from -9trillion-9trillion
     or lower case etc.
      */
     // 1. String
-    String name = new String("zabit");
+   /* String name = new String("zabit");
     System.out.println(name.toUpperCase(Locale.ROOT));
 
 /*  the symbol % is called Modulus.
@@ -134,28 +137,28 @@ finds the ramainder for example 10 % 2 would be 0 because
         and you get 1 remainder.
  */
     // This uses PEMDAS, it does it for you.
-    System.out.println(10 + 7 * 2);
+    /*System.out.println(10 + 7 * 2);
 /* there's a class that actually is for numbers and the
   the class is called Math. side note **
   if there is a dot/  that would mean its a Method.
   it basically shows you more things or certain things you can
   do.
  */
-    System.out.println(Math.abs(-10)); /* this shows
+   /* System.out.println(Math.abs(-10)); /* this shows
         you the absolute value of -10. that would be 10 */
-    System.out.println(Math.max(5, 10));/* this shows you
+   /* System.out.println(Math.max(5, 10));/* this shows you
         the max number between 5 and 10. it would be 10. */
-    System.out.println(Math.pow(5, 2)); /*this basically does
+    /* System.out.println(Math.pow(5, 2)); /*this basically does
         5^2 / 5*5 = 25.
 
         Comparison Operators
        it compares two value */
-    int myage = 14;
+    /* int myage = 14;
     int yourage = 12;
     boolean age = myage < yourage;
       /* boolean is a either true of false statement. it
         returns a true or false statement.*/
-    System.out.println(age);
+    /* System.out.println(age);
     //this will return false since 12 < 14.
       /* you can either do that or just
       System.out.println(myage < yourage); and delete the
@@ -203,6 +206,35 @@ or false statements.
           one boolean value.
          */
 
+            //Methods
+     /* Example:
+     lets say you wanted to multiply these numbers
+     and wanted to keep on going.
+     it would be too much work. this is where methods come in.
+
+      */
+//         int a = 5;
+//         int b = 10;
+//         System.out.println(a * b);
+//
+//    int c = 5;
+//    int d = 10;
+//    System.out.println(c * d);
+
+      //To create you're method we type ...
+      //public static void
+              // then we name it/ lets call it multiply
+        //public static void multiply() {
+          /* to pass value in your method, you
+          would type it in your parenthesis.
+           */
+    //public static void multiply(int a, int b) {
+   // now we want to multiply for example.
+        //System.out.println(a * b);
+    /* now we can replace the int line for somthing else.
+   We replace them for. We just say.
+   See the whole thing in the methods class.
+     */
 
 
 
@@ -215,7 +247,6 @@ or false statements.
 
 
 
-          }
 
 
 
@@ -223,5 +254,3 @@ or false statements.
 
 
 
-
-}
